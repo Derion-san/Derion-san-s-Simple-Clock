e96547637f6627240a1f8c0655d01eac8775b4cc
Zsolt 'Derion-san' M <Derion-san@users.noreply.github.com>
2018-04-23 21:21:22 +0200

    Init
    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+function createWindow () { 
+  var newWidth = 400;
+  var newHeight = 200;
+    
+  mainWindow = new BrowserWindow({frame:false,
+                                  width: newWidth,
+                                  height: newHeight})
+
+  mainWindow.loadURL(`file://${__dirname}/pages/mainPage.html`)
+
+
+  //mainWindow.webContents.openDevTools()   // Open the DevTools?
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
